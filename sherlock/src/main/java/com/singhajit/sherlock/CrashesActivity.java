8e5a970b604ab97e95c415b4fb39f8df6bd0d042
AjitSingh <jeetsingh.ajit@gmail.com>
2017-04-11 22:14:24 +0530

    Initial Commit

---
+++ sherlock/src/main/java/com/singhajit/sherlock/CrashesActivity.java
@@ -0,0 +1,13 @@
+package com.singhajit.sherlock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CrashesActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.crashes_activity);
+  }
+}
