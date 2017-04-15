4e391f73d19fa7fcadf9150d58558a3fe3fa3b49
AjitSingh <jeetsingh.ajit@gmail.com>
2017-04-15 17:33:26 +0530

    Open crash detail on tap of crash notification

--- sherlock/src/main/java/com/singhajit/sherlock/core/investigation/CrimeReporter.java
+++ sherlock/src/main/java/com/singhajit/sherlock/core/investigation/CrimeReporter.java
@@ -2,9 +2,15 @@ package com.singhajit.sherlock.core.investigation;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.singhajit.sherlock.R;
+import com.singhajit.sherlock.crashes.CrashActivity;
+
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 
 public class CrimeReporter {
   private final Context context;
@@ -14,10 +20,21 @@ public class CrimeReporter {
   }
 
   public void report(CrimeViewModel crimeViewModel) {
+    Intent crashActivityIntent = new Intent(context, CrashActivity.class);
+    crashActivityIntent.putExtra(CrashActivity.CRASH_ID, crimeViewModel.getIdentifier());
+
+    TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+    stackBuilder.addParentStack(CrashActivity.class);
+    stackBuilder.addNextIntent(crashActivityIntent);
+
+    PendingIntent pendingIntent = stackBuilder.getPendingIntent(221, FLAG_UPDATE_CURRENT);
+
     Notification notification = new Notification.Builder(context)
         .setContentTitle(crimeViewModel.getPlaceOfCrime())
         .setContentText(crimeViewModel.getDate())
-        .setSmallIcon(R.mipmap.ic_launcher)
+        .setSmallIcon(R.mipmap.ic_stat_sherlock_logo)
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
         .build();
 
     NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
