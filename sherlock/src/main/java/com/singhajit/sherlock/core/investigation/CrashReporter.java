034606e39da7c9fcf2663dd07a2d295c8014cf66
AjitSingh <jeetsingh.ajit@gmail.com>
2017-09-10 18:44:59 +0530

    Adds support for Android O

--- sherlock/src/main/java/com/singhajit/sherlock/core/investigation/CrashReporter.java
+++ sherlock/src/main/java/com/singhajit/sherlock/core/investigation/CrashReporter.java
@@ -1,6 +1,7 @@
 package com.singhajit.sherlock.core.investigation;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -12,16 +13,35 @@ import android.support.v4.content.ContextCompat;
 import com.singhajit.sherlock.R;
 import com.singhajit.sherlock.crashes.activity.CrashActivity;
 
+import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 
 public class CrashReporter {
   private final Context context;
+  private String CHANNEL_ID = "com.singhajit.com.221B";
 
   public CrashReporter(Context context) {
     this.context = context;
   }
 
   public void report(CrashViewModel crashViewModel) {
+    Notification notification = notification(crashViewModel);
+
+    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel = new NotificationChannel(
+          CHANNEL_ID,
+          "Sherlock",
+          IMPORTANCE_DEFAULT
+      );
+      notificationManager.createNotificationChannel(channel);
+    }
+
+    notificationManager.notify(crashViewModel.getIdentifier(), notification);
+  }
+
+  private Notification notification(CrashViewModel crashViewModel) {
     Intent crashActivityIntent = new Intent(context, CrashActivity.class);
     crashActivityIntent.putExtra(CrashActivity.CRASH_ID, crashViewModel.getIdentifier());
 
@@ -42,7 +62,10 @@ public class CrashReporter {
       notificationBuilder.setColor(ContextCompat.getColor(context, R.color.sherlock_colorAccent));
     }
 
-    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(crashViewModel.getIdentifier(), notificationBuilder.build());
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      notificationBuilder.setChannelId(CHANNEL_ID);
+    }
+
+    return notificationBuilder.build();
   }
 }
